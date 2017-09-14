1700ad36ecedb7a95077e7d66bc9c35e83cb0711
Елена Сермягина <fail.lee.smith@gmail.com>
2017-09-14 15:04:40 +0300

    Initial Commit

---
+++ CurrencyConverterProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyConverterProject
+//
+//  Created by Елена Сермягина on 14.09.17.
+//  Copyright © 2017 Елена Сермягина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
